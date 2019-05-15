26b73508b6b7bde21ab5d5ca0854c6e716585ae2
Incfex <kuro@kuroa.me>
2019-05-14 20:05:50 -0400

    Make added option to configure sshd

--- button.sh
+++ button.sh
@@ -1,49 +1,54 @@
 #!/bin/bash
 
 # Processing options
-while getopts ":au:Nz" opt; do
-	case ${opt} in
-		u)
-			usermod -aG sudo $OPTARG
-			;;
-		N)
-			sed -i 's/%sudo	ALL=(ALL:ALL) ALL/%sudo ALL=(ALL:ALL) NOPASSWD:ALL/' /etc/sudoers
-			;;
-		a)
-			apt update
-			apt -y install sudo
-			apt -y install zsh
-			apt -y install build-essential
-			apt -y install vim
-			apt -y install gcc
-			apt -y install gdb
-			apt -y install python
-			apt -y install python3
-			apt -y install nginx
-			apt -y install openssh-server
-			apt -y install curl
-			apt -y install g++
-			apt -y install htop
-			apt -y install tree
-			apt -y install git
-			apt -y install brctl
-			apt -y install isc-dhcp-client
-			apt -y install isc-dhcp-server
-			apt -y upgrade
-			;;
-		z)
-			# Setting up zsh
-			wget http://grml.org/zsh/zshrc
-			mv zshrc /etc/zsh/
-			chsh -s /bin/zsh
-			;;
-		\?)
-			echo "use -u to specify the user you want to add to sudoers\nuse -N to make the sudoers not requiring password\nuse -a to do apt install" 1>&2
-			exit 1
-			;;
-		:)
-			echo "Invalid Option: -$OPTARG requires an argument" 1>&2
-			exit 1
-			;;
-	esac
+while getopts ":au:Nzs" opt; do
+    case ${opt} in
+        u)
+            usermod -aG sudo $OPTARG
+        ;;
+        N)
+            sed -i 's/%sudo	ALL=(ALL:ALL) ALL/%sudo ALL=(ALL:ALL) NOPASSWD:ALL/' /etc/sudoers
+        ;;
+        a)
+            apt update
+            apt -y install sudo
+            apt -y install zsh
+            apt -y install build-essential
+            apt -y install vim
+            apt -y install gcc
+            apt -y install gdb
+            apt -y install python
+            apt -y install python3
+            apt -y install nginx
+            
+            apt -y install curl
+            apt -y install g++
+            apt -y install htop
+            apt -y install tree
+            apt -y install git
+            apt -y install brctl
+            apt -y install isc-dhcp-client
+            apt -y install isc-dhcp-server
+            apt -y upgrade
+        ;;
+        s)
+            apt -y install openssh-server
+            sed -i 's/.\{0,1\}PasswordAuthentication.\{0,5\}$/PasswordAuthentication no/' /etc/ssh/sshd_config
+            systemctl restart sshd
+        ;;
+        z)
+            # Setting up zsh
+            wget http://grml.org/zsh/zshrc
+            mv zshrc /etc/zsh/
+            chsh -s /bin/zsh
+        ;;
+        \?)
+            echo "use -u to specify the user you want to add to sudoers\nuse -N to make the sudoers not requiring password\nuse -a to do apt install" 1>&2
+            exit 1
+        ;;
+        :)
+            echo "Invalid Option: -$OPTARG requires an argument" 1>&2
+            exit 1
+        ;;
+    esac
 done

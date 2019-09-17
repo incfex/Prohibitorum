a23c71c6369a3c796f11f8aea054f4495e4014d4
Incfex <kuro@kuroa.me>
2019-09-17 17:43:35 +0800

    button:added tmux

--- button.sh
+++ button.sh
@@ -14,14 +14,14 @@ while getopts ":au:Nzsh" opt; do
             apt update
             apt -y install sudo
             usermod -aG sudo $OPTARG
-	    sed -i '1 a\Defaults        insults' /etc/sudoers
+	        sed -i '1 a\Defaults        insults' /etc/sudoers
         ;;
         N)
             sed -i 's/%sudo	ALL=(ALL:ALL) ALL/%sudo ALL=(ALL:ALL) NOPASSWD:ALL/' /etc/sudoers
         ;;
         a)
             apt update
-            apt -y install sudo zsh build-essential net-tools vim gcc gdb g++ python python3 curl wget htop tree git bridge-utils mtr-tiny
+            apt -y install sudo zsh build-essential net-tools vim gcc gdb g++ python python3 curl wget htop tree git bridge-utils mtr-tiny tmux
             #apt -y install nginx
             #apt -y install isc-dhcp-client
             #apt -y install isc-dhcp-server
